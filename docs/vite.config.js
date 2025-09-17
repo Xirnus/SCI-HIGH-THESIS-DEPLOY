fbe9651c1a459863fb00a63e80f2376b83d89058
Xirnus <Xernous343@gmail.com>
2025-09-17 18:25:38 +0800

    Revert "Update vite.config.js"
    This reverts commit 73220ccb77c19794aab557bc91ae9a555d962290.

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -9,8 +9,8 @@ export default defineConfig({
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: './index.html',
-        game: './game.html',
+        main: './pages/index.html',
+        game: './pages/game.html',
         leaderboards: './pages/leaderboards.html',
         'professor-dashboard': './pages/professor-dashboard.html',
         developer: './pages/developer.html'
